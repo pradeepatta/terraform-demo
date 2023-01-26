bb36b655dcba69e8e7ddcea2af16ec2cebbddad6
Pradeep Atta <pradeep.atta@gmail.com>
2023-01-26 15:21:14 +0530

    Final

--- main.tf
+++ main.tf
@@ -3,10 +3,10 @@ module "networking" {
   namespace = var.namespace
 }
 
-module "ssh-key" {
-  source    = "./modules/ssh-key"
-  namespace = var.namespace
-}
+//module "ssh-key" {
+//  source    = "./modules/ssh-key"
+//  namespace = var.namespace
+//}
 
 module "ec2" {
   source     = "./modules/ec2"
@@ -14,6 +14,6 @@ module "ec2" {
   vpc        = module.networking.vpc
   sg_pub_id  = module.networking.sg_pub_id
   sg_priv_id = module.networking.sg_priv_id
-  key_name   = module.ssh-key.key_name
+  ##key_name   = module.ssh-key.key_name
 }
 
