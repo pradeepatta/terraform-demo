c7233f2be18f687a2cbd9c8a3577b8848b98b5d2
Pradeep Atta <pradeep.atta@gmail.com>
2023-01-29 12:03:09 +0530

    Final Commit

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "allow_ssh_priv" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    cidr_blocks = ["10.10.0.0/16"]
   }
 
   egress {
